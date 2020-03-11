d3e4e96c6be8e0ef3883c59b70a00b301066ffbc
Marina Poprotskaya <mitsevich.marina@gmail.com>
2020-03-11 17:15:59 +0300

    feat: add slider

--- script.js
+++ script.js
@@ -8,3 +8,65 @@ MENU.addEventListener('click', function(event){
 });
 
 
+(function() {
+
+    const sliderInIt = function(slider) {
+
+        let sliders = slider.querySelectorAll('div.slider-phone');
+
+        sliders[0].classList.add('active-slider');
+
+        let ArrowNext = document.createElement('div');
+        ArrowNext.classList.add('right');
+
+        let ArrowPrev = document.createElement('div');
+        ArrowPrev.classList.add('left');
+
+        console.log(ArrowNext, ArrowPrev);
+
+        slider.appendChild(ArrowNext);
+        slider.appendChild(ArrowPrev);
+
+        ArrowNext.addEventListener('click', function(){
+            let activeElem = slider.querySelector('div.active-slider');
+
+            if(activeElem !=null) {
+                activeElem.classList.remove('active-slider');
+                let nextElem = activeElem.nextElementSibling.classList.add('active-slider');
+                if(nextElem == null) {
+                    activeElem.classList.remove('active-slider');
+                    activeElem.previousElementSibling.classList.add('active-slider');
+                }
+            }
+        });
+
+        ArrowPrev.addEventListener('click', function(){
+            let activeElem = slider.querySelector('div.active-slider');
+
+            if(activeElem !=null) {
+                activeElem.classList.remove('active-slider');
+                let nextElem = activeElem.nextElementSibling.classList.add('active-slider');
+                if(nextElem == null) {
+                    activeElem.classList.remove('active-slider');
+                    activeElem.previousElementSibling.classList.add('active-slider');
+                }
+            }
+        });
+
+
+    };
+
+    this.slider = function(selector){
+        
+        let slider = document.querySelector(selector);
+
+        if (!slider) {
+            console.log ('Slider not found');
+            return false;
+        }
+
+        sliderInIt(slider); 
+    };
+}());
+slider('#slider');
+
