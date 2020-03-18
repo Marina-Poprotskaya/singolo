1232f70869515f6f7c35eec60fe31002424ce14c
Marina Poprotskaya <mitsevich.marina@gmail.com>
2020-03-18 21:11:07 +0300

    feat: add scroll task

--- script.js
+++ script.js
@@ -10,6 +10,28 @@ MENU.addEventListener('click', function (event) {
 
 
 
+document.addEventListener('scroll', onScroll);                                    //Scroll
+
+function onScroll() {
+    let curPos = window.scrollY+document.getElementById('home').offsetHeight;
+    let divs = document.querySelectorAll('.scroll');
+    let links = document.querySelectorAll('#menu a');
+    divs.forEach(function (elem) {
+        if (elem.offsetTop <= curPos && (elem.offsetTop + elem.offsetHeight) > curPos) {
+            links.forEach(function (a) {
+                a.classList.remove('active');
+                if (a.getAttribute('href').substring(1) === elem.getAttribute('id')) {
+                    a.classList.add('active');
+                }
+            });
+        };
+    });
+};
+
+
+
+
+
 (function () {                                                                   // Слайдер
 
     const sliderInIt = function (slider) {
@@ -45,21 +67,21 @@ MENU.addEventListener('click', function (event) {
 
         ArrowPrev.addEventListener('click', function () {
             let activeElem = slider.querySelector('div.active-slider');
-            let prevElem = activeElem.previousElementSibling;          
-            if (activeElem != null) {  
+            let prevElem = activeElem.previousElementSibling;
+            if (activeElem != null) {
                 if (!prevElem) {
                     activeElem.classList.remove('active-slider');
-                    sliders[sliders.length-1].classList.add('active-slider');
-                }         
+                    sliders[sliders.length - 1].classList.add('active-slider');
+                }
                 else if (prevElem.classList.contains('slider-phone')) {
-              
-                 activeElem.classList.remove('active-slider');
-                 prevElem.classList.add('active-slider');
-             }
-             else {
-                 activeElem.classList.remove('active-slider');
-                 sliders[sliders.length-1].classList.add('active-slider');
-             }
+
+                    activeElem.classList.remove('active-slider');
+                    prevElem.classList.add('active-slider');
+                }
+                else {
+                    activeElem.classList.remove('active-slider');
+                    sliders[sliders.length - 1].classList.add('active-slider');
+                }
             }
         });
     };
@@ -161,25 +183,25 @@ const close_button = document.getElementById('close-btn');
 const form = document.getElementById('form');
 
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
     event.preventDefault();
     const subject = document.getElementById('subject').value.toString();
     const textarea = document.getElementById('textarea_message').value.toString();
     if (subject == '' && textarea == '') {
-    document.getElementById('result').innerHTML = 'Письмо отправлено <br/> Без темы <br/> Без описания';
-    } else if (subject =='') {
-    document.getElementById('result').innerHTML = 'Письмо отправлено <br/> Без темы  <br/> Описание: ' + textarea;
+        document.getElementById('result').innerHTML = 'Письмо отправлено <br/> Без темы <br/> Без описания';
+    } else if (subject == '') {
+        document.getElementById('result').innerHTML = 'Письмо отправлено <br/> Без темы  <br/> Описание: ' + textarea;
     } else if (textarea == '') {
-    document.getElementById('result').innerHTML = 'Письмо отправлено <br/> Тема: ' + subject + '<br/> Без описания';
+        document.getElementById('result').innerHTML = 'Письмо отправлено <br/> Тема: ' + subject + '<br/> Без описания';
     } else document.getElementById('result').innerHTML = 'Письмо отправлено <br/> Тема: ' + subject + '<br/> Описание: ' + textarea;
     document.getElementById('pop-up_form').classList.remove('hidden');
 });
 
-    close_button.addEventListener('click', function () {
+close_button.addEventListener('click', function () {
     form.reset();
     console.log(result);
     document.getElementById('pop-up_form').classList.add('hidden')
-    });
+});
 
 
 
